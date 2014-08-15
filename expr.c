e50e1096b32de5b76699219dcd46428acd2d0cee
lmy441900 <826531041@qq.com>
2014-08-15 15:38:08 +0800

    Updated 20140815

--- expr.c
+++ expr.c
@@ -2,124 +2,47 @@
 # include <stdlib.h>
 # include <string.h>
 
-# define LENGTH 512
-
-# define op_left_assoc( c ) ( c == '+' || c == '-' || c == '/' || c == '*' || c == '%' )
-# define is_operator( c )   ( c == '+' || c == '-' || c == '/' || c == '*' || c == '!' || c == '%' || c == '=' )
-# define is_function( c )   ( c >= 'A' && c <= 'Z' )
-# define is_ident( c )      ( ( c >= '0' && c <= '9' ) || ( c >= 'a' && c <= 'z' ) )
-
-int opr_preced ( const char c );
-
-typedef struct /* For operators */
-{
-    int  top;
-    char *a;
-} stack;
-
-
-
-
 int main ( void )
 {
-    stack opr = { 0, NULL }, /* Operators */
-          suf = { 0, NULL }; /* Suffix notation */
-    char  *s = ( char * ) calloc ( LENGTH, sizeof ( char ) ),    /* Input */
-          *outq = ( char * ) calloc ( LENGTH, sizeof ( char ) ); /* Output Queue */
-    int   tmp = 0,
-          p = 0, /* "Pointer" for *outq  */
-          t = 0; /* Total */
+    int   t = 0,
+          tmp = 0,
+          *num = ( int * ) calloc ( 512, sizeof ( int ) );
+    char  *expr = ( char * ) calloc ( 512, sizeof ( char ) );
+    FILE  *fi = fopen ( "expr.in", "r" ),
+          *fo = fopen ( "expr.out", "w" );
     
-    puts ( "Input an infix notation:" );
-    scanf ( "%s", s );
-    opr.a = ( char * ) calloc ( LENGTH, sizeof ( char ) );
+    fscanf ( fi, "%s", expr );
     
-    /* Go You!*/
-    for ( tmp = 0; tmp < strlen ( s ); tmp++ )
+    for ( tmp = 0; tmp < strlen ( expr ); tmp++ )
     {
-        if ( is_ident ( s[tmp] ) )
-        {
-            outq[p] = s[tmp];
-            p++;
-        }
-        else
-        if ( is_operator ( s[tmp] ) )
-        {
-            if ( is_operator ( opr.a[opr.top] ) && opr_preced ( opr.a[opr.top] ) > s[tmp] )
-            {
-                /* Pop */
-                outq[p] = opr.a[opr.top - 1];
-                p++;
-                opr.top--;
-            }
-            else
-            {
-                opr.a[opr.top] = s[tmp];
-                opr.top++;
-            }
-        }
-        /* 先不考虑括号和函数什么的 */
+        if ( ( expr[tmp] >= '0' ) && ( expr[tmp] <= '9' ) )
+            num[tmp] = ( int ) expr[tmp] - '0';
     }
     
-    /* Put the operators inside stack into output queue */
-    for ( tmp = opr.top; tmp >= 0; tmp-- )
+    for ( tmp = 0; tmp < strlen ( expr ); tmp++ )
     {
-        outq[p] = opr.a[opr.top - 1];
-        p++;
-        opr.top--;
-    }
-    
-    /* Calculate */
-    p = 0;
-    suf.a = ( char * ) calloc ( LENGTH, sizeof ( char ) );
-    for ( tmp = 0; tmp < strlen ( outq ); tmp++ )
-    {
-        if ( is_ident ( outq[p] ) )
+        if ( expr[tmp] == '*' )
         {
-            /* Push */
-            suf.a[suf.top] = outq[p];
-            p++;
-            suf.top++;
-        }
-        else
-        if ( is_operator ( outq[p] ) )
-        {
-            /* TODO: God knows whether there are 2 numbers or not... */
-            switch ( outq[p] )
-            {
-                case '+':
-                    t = suf.a[suf.top - 1] + suf.a[suf.top - 2];
-                    break;
-                case '-':
-                    break; /* ... */
-                case '*':
-                    t = suf.a[suf.top - 1] * suf.a[suf.top - 2];
-                    break;
-                case '/':
-                    break; /* ... */
-            }
+            num[tmp + 1] = num[tmp - 1] * num[tmp + 1];
+            num[tmp - 1] = 0;
         }
     }
     
-    printf ( "%d", t );
+    for ( tmp = 0; tmp < strlen ( expr ); tmp++ )
+    {
+        if ( expr[tmp] == '+' )
+        {
+            num[tmp + 1] = num[tmp - 1] + num[tmp + 1];
+            num[tmp - 1] = 0;
+        }
+    }
     
-    return 0;
-}
+    for ( tmp = 0; tmp < strlen ( expr ); tmp++ )
+        t += num[tmp];
+    
+    fprintf ( fo, "%d", t );
 
-int opr_preced ( const char c )
-{
-    switch ( c )
-    {
-        case '!':
-            return 4;
-        case '*':
-        case '/':
-        case '%':
-            return 3;
-        case '+': case '-':
-            return 2;
-        case '=':
-            return 1;
-    }
+    fclose ( fi );
+    fclose ( fo );
     return 0;
 }
